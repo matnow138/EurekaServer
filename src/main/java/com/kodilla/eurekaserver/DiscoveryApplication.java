20de4ef36875bb6e59bbc1483ad53a71672453dd
unknown <mateuszp.nowak@gmail.com>
2023-01-17 21:11:59 +0100

    EurekaServer 0.9

--- src/main/java/com/kodilla/eurekaserver/DiscoveryApplication.java
+++ src/main/java/com/kodilla/eurekaserver/DiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.kodilla.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class DiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 
 }
